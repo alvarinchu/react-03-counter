416a08912e2a4b995ab39fa44dba2edfa02c6267
alvarinchu <alvarinchu@gmail.com>
2021-06-19 07:44:16 +0000

    commit1

---
+++ react-03-counter/src/MiPrimeraApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MiPrimeraApp = () => {
+
+    const html = 
+        <>
+            <h1>Hola Mundo!</h1>
+            <p>Viva España</p>
+        </>;
+
+    return html;
+}
+
+export default MiPrimeraApp;
---
+++ react-03-counter/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MiPrimeraApp from './MiPrimeraApp';
+import './index.css';
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render(<MiPrimeraApp/>, divRoot);
